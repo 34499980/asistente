fc659d72551c09ef3ee85ef9bf6b29b5e5039142
abrenman <aribrenman@gmail.com>
2019-05-17 12:35:08 -0300

    Porcentaje de volumen

--- app/src/main/java/com/android/asistente/asistente/Entities/Sound.java
+++ app/src/main/java/com/android/asistente/asistente/Entities/Sound.java
@@ -6,14 +6,15 @@ import android.media.AudioManager;
 import com.android.asistente.asistente.MainActivity;
 
 public class Sound {
-
+int val;
 
     //Using volume control UI visibility
 //To increase media player volume
     public void setVolumen(int Volumen){
         try {
+            val=(Volumen * 15) / 100;
             AudioManager audioManager = (AudioManager) MainActivity.getContext().getSystemService(Context.AUDIO_SERVICE);
-            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, Volumen, 0);
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, val, 0);
         }catch (Exception ex){
             throw ex;
         }
