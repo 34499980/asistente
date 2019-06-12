44d6988c5006a497456f8569dc23a4343209d264
abrenman <aribrenman@gmail.com>
2019-06-12 15:26:19 -0300

    Saco acentos

--- app/src/main/java/com/android/asistente/asistente/Business/Contacts.java
+++ app/src/main/java/com/android/asistente/asistente/Business/Contacts.java
@@ -1,5 +1,6 @@
 package com.android.asistente.asistente.Business;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -7,6 +8,7 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 
 import com.android.asistente.asistente.Entities.Phone;
+import com.android.asistente.asistente.Helper.General;
 import com.android.asistente.asistente.MainActivity;
 
 import java.util.ArrayList;
@@ -17,21 +19,26 @@ public class Contacts extends AppCompatActivity {
         String number="";
         Phone phone;
         Boolean bFlag=false;
+        name = General.CleanString(name.toLowerCase());
         ArrayList<Phone> listContacs = new ArrayList<Phone>();
-        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        String[] projection    = new String[] {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.NUMBER};
+       // Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        //String[] projection    = new String[] {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+         //       ContactsContract.CommonDataKinds.Phone.NUMBER};
 
-        Cursor people = MainActivity.getContext().getContentResolver().query(uri, projection, null, null, null);
-
+        //Cursor people = MainActivity.getContext().getContentResolver().query(uri, projection, null, null, null);
+        //Prueba contactos
+        ContentResolver cr = MainActivity.getContext().getContentResolver();
+        Cursor people = cr.query(ContactsContract.Contacts.CONTENT_URI, null,
+                null, null, null);
+        //Fin prueba
         int indexName = people.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
         int indexNumber = people.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
 
         //people.moveToFirst();
         people.moveToNext();
         while(people.moveToNext() && !people.getString(indexName).toLowerCase().equals(name)) {
-
-            String valueName = people.getString(indexName).toLowerCase();
+            //Limpio toda la cadea de acentos
+            String valueName = General.CleanString(people.getString(indexName).toLowerCase());
             if(valueName.contains(name)){
                 if(listContacs.isEmpty()){
                     phone = new Phone();
--- app/src/main/java/com/android/asistente/asistente/Helper/General.java
+++ app/src/main/java/com/android/asistente/asistente/Helper/General.java
@@ -14,6 +14,7 @@ import com.android.asistente.asistente.Entities.Phone;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.Normalizer;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -27,4 +28,19 @@ import static android.support.v4.content.ContextCompat.startActivity;
 
 public class General extends Activity {
  public static List<Phone> list;
+
+ public static String CleanString(String cadena) {
+  String limpio =null;
+  if (cadena !=null) {
+   String valor = cadena;
+   valor = valor.toUpperCase();
+   // Normalizar texto para eliminar acentos, dieresis, cedillas y tildes
+   limpio = Normalizer.normalize(valor, Normalizer.Form.NFD);
+   // Quitar caracteres no ASCII excepto la enie, interrogacion que abre, exclamacion que abre, grados, U con dieresis.
+   limpio = limpio.replaceAll("[^\\p{ASCII}(N\u0303)(n\u0303)(\u00A1)(\u00BF)(\u00B0)(U\u0308)(u\u0308)]", "");
+   // Regresar a la forma compuesta, para poder comparar la enie con la tabla de valores
+   limpio = Normalizer.normalize(limpio, Normalizer.Form.NFC).replace('k','c');
+  }
+  return limpio;
+ }
 }
