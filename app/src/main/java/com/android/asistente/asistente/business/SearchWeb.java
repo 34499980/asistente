a1d1df98cbd645f609a932e231ca61abd2c4ce2d
34499980 <ab34499980ab>
2019-08-19 14:57:55 -0300

    Inicio de busqueda en google

--- app/src/main/java/com/android/asistente/asistente/business/SearchWeb.java
+++ app/src/main/java/com/android/asistente/asistente/business/SearchWeb.java
@@ -1,44 +1,90 @@
 package com.android.asistente.asistente.business;
 
-import com.android.asistente.asistente.Helper.Log;
-import com.loopj.android.http.HttpGet;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import cz.msebera.android.httpclient.Header;
-import cz.msebera.android.httpclient.HttpResponse;
-import cz.msebera.android.httpclient.HttpStatus;
-import cz.msebera.android.httpclient.StatusLine;
-import cz.msebera.android.httpclient.client.ClientProtocolException;
-import cz.msebera.android.httpclient.client.HttpClient;
-import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+
+
+import java.io.BufferedReader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 
 public class SearchWeb {
-    public void search(String query){
-        try{
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpResponse response = httpclient.execute(new HttpGet("https://google.com/search?q="+query));
-            StatusLine statusLine = response.getStatusLine();
-            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                Header[] headers = response.getAllHeaders();
-                for (Header item: headers) {
-                    item.getValue();
-                    item.getElements();
-                    item.getName();
 
-                }
-                //Here I do something with the Date String
-                //System.out.println(dateStr);
 
-            } else{
-                //Closes the connection.
-                response.getEntity().getContent().close();
-                throw new IOException(statusLine.getReasonPhrase());
-            }
-        }catch (ClientProtocolException e) {
-            Log.appendLog( e.getMessage());
-        }catch (IOException e) {
-            Log.appendLog(e.getMessage());
+    public class HttpRequest {
+
+        public  synchronized String doRequest(String urlRequest) throws Exception
+        {
+            String jsonRespuesta = "";
+
+            HttpURLConnection urlConnection= null;
+            URL url = new URL(urlRequest);
+            urlConnection=(HttpURLConnection)url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            jsonRespuesta = convertStreamToString(urlConnection.getInputStream());
+
+            return jsonRespuesta;
         }
+
+        private  String convertStreamToString(InputStream is) throws Exception {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            return sb.toString();
+        }
+
+    }
+    public class UrlFactory {
+
+        private static final String url = "https://www.googleapis.com/customsearch/v1?";
+        private static final  String KEY = "AIzaSyD0xPBxooSwcSxb2J16vPJE7HQtvxjqyPk";
+        private static final String cx = "016697470152593144145:catp7rewrfa";
+
+        public  String buildUrl(String keywords) throws Exception{
+            return url +"?key=" +KEY+ "&cx=" + cx + "&q=" + URLEncoder.encode(keywords);
+        }
+
+    }
+    public class ResultDeserializer {
+
+       /* public void deserialize(String object) throws JSONException {
+            //Result result = new Result();
+
+            final List<Item> items = new ArrayList<Item>();
+
+            JSONObject jSonObject = new JSONObject(object);
+            JSONArray jSonObjectArray = jSonObject.getJSONArray("items");
+            for (int count = 0; count < jSonObjectArray.length(); count++) {
+                JSONObject jsonItem = (JSONObject) jSonObjectArray.get(count);
+
+                Item item = new Item();
+                item.setTitle(jsonItem.getString("title"));
+                item.setHtmlTitle(jsonItem.getString("htmlTitle"));
+                item.setLink(jsonItem.getString("link"));
+                item.setDisplayLink(jsonItem.getString("displayLink"));
+                item.setSnippet(jsonItem.getString("snippet"));
+                item.setHtmlSnippet(jsonItem.getString("htmlSnippet"));
+
+                items.add(item);
+            }
+
+            result.setItems(items);
+
+            return result;
+        }*/
     }
 }
